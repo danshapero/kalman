8422ed6ff600f5cd7cb8e3746e365c17461698f3
danshapero <shapero.daniel@gmail.com>
2015-09-23 14:32:54 -0700

    Fixed row/col vector Lorenz

--- systems.jl
+++ systems.jl
@@ -1,8 +1,8 @@
 
 function Lorenz(sigma, rho, beta)
-    function F(t, q)
-        [sigma * (q[2] - q[1]);
-         q[1] * (rho - q[3]) - q[2];
+    function F(t::Float64, q::Vector{Float64})
+        [sigma * (q[2] - q[1]),
+         q[1] * (rho - q[3]) - q[2],
          q[1] * q[2] - beta * q[3]]
     end
 end
